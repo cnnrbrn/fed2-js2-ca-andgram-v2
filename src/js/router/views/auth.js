bcee9f7d1fc7ed926bc29a64e49fb4ee7a8b3973
andgram <145982786+andgram@users.noreply.github.com>
2024-09-13 14:49:37 +0200

    Add authentication check in auth.js and redirect authenticated users to Home page

--- src/js/router/views/auth.js
+++ src/js/router/views/auth.js
@@ -1,1 +1,12 @@
-alert("Auth Index Page");
+// Function to check if the user is authenticated
+function redirectIfAuthenticated() {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+      window.location.href = '/'; // Redirect if token exists
+    }
+  }
+  
+  // Run the redirection check
+  redirectIfAuthenticated();
+  
