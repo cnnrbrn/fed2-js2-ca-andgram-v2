1748f8599f474da03dfcd47f4e8921096ecd83fa
andgram <145982786+andgram@users.noreply.github.com>
2024-09-20 10:56:21 +0200

    Delete unnecessary files

--- src/js/utilities/getId.js
+++
@@ -1,8 +0,0 @@
-export function getIdFromUrl() {
-    // Get post ID from URL params
-    const postId = new URLSearchParams(window.location.search).get('id');
-    if (!postId) {
-      console.error('Post ID not found');
-      return;
-    }
-  }
